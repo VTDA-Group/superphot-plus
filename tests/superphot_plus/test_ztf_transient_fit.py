9f9b9a0a950e9424b2a118b7afce44cbebbe314b
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2023-07-20 11:03:10 -0400

    Simplify the test further

--- tests/superphot_plus/test_ztf_transient_fit.py
+++ tests/superphot_plus/test_ztf_transient_fit.py
@@ -12,6 +12,7 @@ def test_dynesty_single_file(tmp_path, single_ztf_lightcurve_compressed):
         skip_if_exists=False,
         rstate=np.random.default_rng(9876),
     )
+    assert len(sample_mean) == 14
 
     output_file = os.path.join(tmp_path, "ZTF22abvdwik_eqwt_dynesty.npz")
     assert os.path.exists(output_file)
@@ -19,25 +20,3 @@ def test_dynesty_single_file(tmp_path, single_ztf_lightcurve_compressed):
     params = np.load(output_file)["arr_0"]
     ## could be between ~600 and ~800, and can vary based on hardware.
     assert 600 <= len(params) <= 800
-
-    # Check that the same means are somewhat close (within 25% relative value).
-    # Despite setting the the random seed, we still need to account for hardware
-    # based differences.
-    expected = [
-        1035.0,
-        0.005,
-        13.5,
-        -4.8,
-        4.0,
-        23.4,
-        0.03,
-        1.1,
-        1.0,
-        1.0,
-        1.0,
-        0.96,
-        0.56,
-        0.87,
-    ]
-    assert np.all(np.isclose(sample_mean, expected, rtol=.25))
-
