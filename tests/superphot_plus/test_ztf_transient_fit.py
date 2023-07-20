dc00300a0b7de04d7378585e9def28e253ff955d
Jeremy Kubica <104161096+jeremykubica@users.noreply.github.com>
2023-07-20 10:23:12 -0400

    Address PR comments

--- tests/superphot_plus/test_ztf_transient_fit.py
+++ tests/superphot_plus/test_ztf_transient_fit.py
@@ -7,11 +7,6 @@ from superphot_plus.ztf_transient_fit import dynesty_single_file
 def test_dynesty_single_file(tmp_path, single_ztf_lightcurve_compressed):
     """Just test that we generated a new file with fits"""
 
-    # Set a random seed to avoid flaky tests. WARNING: Do not do this
-    # for scientific runs as it will not produce correct results.
-    assert "DYNESTY_RANDOM_SEED" not in os.environ
-    os.environ["DYNESTY_RANDOM_SEED"] = "2023"
-
     sample_mean = dynesty_single_file(
         single_ztf_lightcurve_compressed,
         tmp_path,
@@ -45,11 +40,4 @@ def test_dynesty_single_file(tmp_path, single_ztf_lightcurve_compressed):
         0.56,
         0.87,
     ]
-    assert len(expected) == len(sample_mean)
-    for i in range(len(expected)):
-        relative_diff = abs(expected[i] - sample_mean[i]) / max(abs(expected[i]), 0.1)
-        assert relative_diff < 0.1
-
-    # Unset the random seed for consistency.
-    del os.environ["DYNESTY_RANDOM_SEED"]
-    assert "DYNESTY_RANDOM_SEED" not in os.environ
+    assert np.all(np.isclose(sample_mean, expected, rtol=.1))
