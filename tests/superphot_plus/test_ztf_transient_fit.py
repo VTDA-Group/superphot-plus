27fd2e5d572d29b8014a4a681cf851adb15d4089
delucchi-cmu <delucchi@andrew.cmu.edu>
2023-08-02 10:28:18 -0400

    Add unit test for run_curve_fit; add multiband.

--- tests/superphot_plus/test_ztf_transient_fit.py
+++ tests/superphot_plus/test_ztf_transient_fit.py
@@ -2,7 +2,7 @@ import os
 
 import numpy as np
 
-from superphot_plus.ztf_transient_fit import dynesty_single_file
+from superphot_plus.ztf_transient_fit import dynesty_single_file, run_curve_fit
 
 
 def test_dynesty_single_file(tmp_path, single_ztf_lightcurve_compressed):
@@ -27,5 +27,14 @@ def test_dynesty_single_file(tmp_path, single_ztf_lightcurve_compressed):
     # additional variations.
     expected = [1035.0, 0.005, 13.5, -4.8, 4.0, 23.4, 0.03, 1.1, 1.0, 1.0, 1.0, 0.96, 0.56, 0.87, -5.43]
     assert len(expected) == len(sample_mean)
-    for i in range(len(expected)):
-        assert np.all(np.isclose(sample_mean, expected, rtol=0.5))
+    assert np.all(np.isclose(sample_mean, expected, rtol=0.5))
+
+
+def test_run_curve_fit(single_ztf_lightcurve_compressed, tmp_path):
+    """Change detection test on returned curve fit."""
+    results = run_curve_fit(single_ztf_lightcurve_compressed, tmp_path, plot=False)
+
+    expected_g = [1089.22, 0, 1.1282, -5.412, 0.6277, 1.146347]
+    expected_r = [1082.44, 0.01575061, 1.297617, -4.5240395, 0.507503, 1.2562]
+    assert np.all(np.isclose(results["g"], expected_g, rtol=0.01))
+    assert np.all(np.isclose(results["r"], expected_r, rtol=0.01))
