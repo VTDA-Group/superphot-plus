b0e41217c3a6349f6974053cdd232abe7b9b45e3
Kaylee de Soto <kdesoto@g.harvard.edu>
2023-08-05 19:11:57 -0400

    Added ZTF survey check

--- tests/superphot_plus/test_surveys.py
+++ tests/superphot_plus/test_surveys.py
@@ -27,3 +27,21 @@ def test_create():
     # test not enough wavelengths defined = NOT OK
     with pytest.raises(AssertionError):
         test_invalid_survey = Survey("Test2", ztf_priors, {"r": 5000.0})
+        
+        
+def test_ztf():
+    """Test ZTF Survey generation.
+    """
+    ztf_survey = Survey.ZTF()
+    assert ztf_survey.name == "ZTF"
+    assert len(ztf_survey.priors.bands) == 2
+    assert ("r" in ztf_survey.wavelengths) and ("g" in ztf_survey.wavelengths)
+    assert len(ztf_survey.wavelengths) == 2
+    
+    # confirm band order self-consistent
+    if np.array_equal(ztf_survey.priors.ordered_bands, ["r", "g"]):
+        assert np.array_equal(ztf_survey.get_ordered_wavelengths(), [6173.23, 4741.64])
+    elif np.array_equal(ztf_survey.priors.ordered_bands, ["g", "r"]):
+        assert np.array_equal(ztf_survey.get_ordered_wavelengths(), [4741.64, 6173.23])
+    else:
+        assert False
