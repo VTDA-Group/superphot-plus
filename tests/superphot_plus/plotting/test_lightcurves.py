3c659631808b203fb4bf0f18c66ed8a7d63bd250
Kaylee de Soto <kdesoto@g.harvard.edu>
2023-08-09 13:09:49 -0400

    Renamed some test files for less ambiguity

--- tests/superphot_plus/plotting/test_lightcurves.py
+++
@@ -1,6 +0,0 @@
-import os
-
-import numpy as np
-import pytest
-
-from superphot_plus.plotting.lightcurves import *
---
+++ tests/superphot_plus/plotting/test_plot_lightcurves.py
@@ -0,0 +1,12 @@
+import os
+
+import numpy as np
+import pytest
+
+from superphot_plus.plotting.lightcurves import (
+    plot_lc_fit,
+    plot_sampling_lc_fit,
+    plot_sampling_lc_fit_numpyro,
+    plot_lightcurve_clipping,
+)
+
