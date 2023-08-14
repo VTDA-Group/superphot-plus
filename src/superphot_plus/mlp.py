Fixed some small bugs, added normalization arrays to ModelConfig initialization, added save/load functions

src/superphot_plus/mlp.py
@@ -6,6 +6,7 @@ import random
 import time
 from dataclasses import dataclass
 from typing import List
+import json
 
 import numpy as np
 import torch
@@ -36,11 +37,48 @@ class ModelConfig:
     output_dim: int
     neurons_per_layer: int
     num_hidden_layers: int
+    
+    normalization_means: List[float]
+    normalization_stddevs: List[float]
+    
 
     device: torch.device = torch.device("cpu")
 
     def __iter__(self):
-        return iter((self.input_dim, self.output_dim, self.neurons_per_layer, self.num_hidden_layers))
+        return iter((
+            self.input_dim,
+            self.output_dim,
+            self.neurons_per_layer,
+            self.num_hidden_layers,
+        ))
+    
+    def save(self, filename):
+        """Save configuration data to a JSON file.
+        """
+        data_dict = {
+            'config': [
+                self.input_dim,
+                self.output_dim,
+                self.neurons_per_layer,
+                self.num_hidden_layers
+            ],
+            'normalization_means': self.normalization_means,
+            'normalization_stddevs': self.normalization_stddevs
+        }
+        with open(filename, 'w') as f:
+            json.dump(data_dict, f)
+        
+    @classmethod
+    def load(cls, filename):
+        """Load configuration data from a JSON file.
+        """
+        with open(filename, "r") as f:
+            data_dict = json.load(f)
+        return ModelConfig(
+            *data_dict['config'],
+            data_dict['normalization_means'],
+            data_dict['normalization_stddevs'],
+        )
 
 
 @dataclass
@@ -249,6 +287,8 @@ class MLP(nn.Module):
             test_group_idxs,
         ) = self.data
 
+        config_path = os.path.join(models_dir, f"superphot-config-{test_names[0]}.json")
+        self.config.save(config_path)
         model_path = os.path.join(models_dir, f"superphot-model-{test_names[0]}.pt")
 
         train_iterator = DataLoader(train_data, shuffle=True, batch_size=BATCH_SIZE)
@@ -361,6 +401,7 @@ class MLP(nn.Module):
                 x = x.to(self.config.device)
                 y = y.to(self.config.device)
 
+                y_pred, _ = self(x)
                 loss = self.criterion(y_pred, y)
 
                 acc = calculate_accuracy(y_pred, y)
@@ -512,7 +553,11 @@ class MLP(nn.Module):
             Sums to 1 along each row.
         """
         fit_params_2d = np.atleast_2d(fit_params)  # cast to 2D if only 1 light curve
-        test_features, _, _ = normalize_features(fit_params_2d, MEANS_TRAINED_MODEL, STDDEVS_TRAINED_MODEL)
+        test_features, _, _ = normalize_features(
+            fit_params_2d,
+            self.config.normalization_means,
+            self.config.normalization_stddevs,
+        )
         test_data = TensorDataset(torch.Tensor(test_features))
         test_iterator = DataLoader(test_data, batch_size=32)
         _, probs = self.get_predictions_from_fit_params(test_iterator)
@@ -540,7 +585,7 @@ class MLP(nn.Module):
         return model
 
     @classmethod
-    def load(cls, filename, config, data=None):
+    def load(cls, filename, config_filename, data=None):
         """Load a trained MLP for subsequent classification of new objects.
 
         Parameters
@@ -549,6 +594,7 @@ class MLP(nn.Module):
             Where the trained MLP is stored.
         config : ModelConfig
             Includes (in order): input_size, output_size, n_neurons, n_hidden.
+            Also includes normalization means and standard deviations.
         data : ModelData
             Training, testing and validation data.
 
@@ -557,6 +603,7 @@ class MLP(nn.Module):
         torch.nn.Module
             The pre-trained MLP object.
         """
+        config = ModelConfig.load(config_filename)
         model = MLP.create(config, data)  # set up empty multi-layer perceptron
         model.load_state_dict(torch.load(filename))  # load trained state dict to the MLP
-        return model
+        return model, config.normalization_means, config.normalization_stddevs