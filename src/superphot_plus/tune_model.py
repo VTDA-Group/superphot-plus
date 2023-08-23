2abba69e2845d59aa6b0ef5bc468ff05da8ac547
Sandro Campos <scampos@andrew.cmu.edu>
2023-08-23 13:44:53 -0400

    Improve documentation

--- src/superphot_plus/tune_model.py
+++ src/superphot_plus/tune_model.py
@@ -1,48 +1,49 @@
-import numpy as np
+import json
 import os
 
+import numpy as np
 import ray
 from joblib import Parallel, delayed
 from ray import tune
 from ray.air import session
 from ray.tune import CLIReporter
+from ray.tune.search.optuna import OptunaSearch
 from sklearn.model_selection import train_test_split
 from superphot_plus.classify_ztf import adjust_log_dists
 from superphot_plus.file_paths import METRICS_DIR, MODELS_DIR
-from superphot_plus.file_utils import get_posterior_samples
+from superphot_plus.format_data_ztf import import_labels_only, generate_K_fold, \
+    oversample_using_posteriors, normalize_features
 from superphot_plus.model.classifier import SuperphotClassifier
 from superphot_plus.model.config import ModelConfig
 from superphot_plus.model.data import TrainData
-
 from superphot_plus.supernova_class import SupernovaClass as SnClass
-from superphot_plus.format_data_ztf import import_labels_only, generate_K_fold, \
-    oversample_using_posteriors, normalize_features
 from superphot_plus.utils import create_dataset
-from ray.tune.search.optuna import OptunaSearch
 
 ####################################
-######## Data configuration ########
-####################################
 
+DATA_DIR = "data"
 SAMPLER = "dynesty"
-FITS_DIR = "data/dynesty_fits"
-INPUT_CSVS = ["data/training_set.csv"]
+
+FITS_DIR = f"{DATA_DIR}/dynesty_fits"
+INPUT_CSVS = [f"{DATA_DIR}/training_set.csv"]
+BEST_CONFIG_FILE = f"{DATA_DIR}/best_config.json"
 
 ####################################
 
 def run_tune_params(config):
-    """Estimates the model performance for each hyperparameter set.
-
-    Reports the mean metric value for each fold.
+    """Estimates the model performance for each hyperparameter set,
+    reporting the mean validation loss and accuracy for each fold.
 
     Parameters
     ----------
-    config The hyperparameters under search
-    num_epochs The number of epochs for training
-    metric The metric to estimate model performance
+    config The classifier hyperparameters
     """
     # Run Tune in the project's working directory.
     os.chdir(os.environ["TUNE_ORIG_WORKING_DIR"])
+
+    # Create output folders.
+    os.makedirs(MODELS_DIR, exist_ok=True)
+    os.makedirs(METRICS_DIR, exist_ok=True)
 
     allowed_types = ["SN Ia", "SN II", "SN IIn", "SLSN-I", "SN Ibc"]
     output_dim = len(allowed_types)
@@ -59,31 +60,22 @@ def run_tune_params(config):
     # Set aside 10% of data for testing.
     names, test_names, labels, test_labels = train_test_split(names, labels, test_size=0.1)
 
-    test_classes = SnClass.get_classes_from_labels(test_labels)
-
-    test_features = []
-    test_classes_os = []
-    test_group_idxs = []
-    test_names_os = []
-
-    for i, test_name in enumerate(test_names):
-        test_posts = get_posterior_samples(test_name, FITS_DIR, SAMPLER)
-        test_features.extend(test_posts)
-        test_classes_os.extend([test_classes[i]] * len(test_posts))
-        test_names_os.extend([test_names[i]] * len(test_posts))
-        if len(test_group_idxs) == 0:
-            start_idx = 0
-        else:
-            start_idx = test_group_idxs[-1][-1] + 1
-        test_group_idxs.append(np.arange(start_idx, start_idx + len(test_posts)))
-
-    test_data = (np.array(test_features), test_classes, test_names, test_group_idxs)
-
     # Generate K-folds for the remaining data.
     kfold = generate_K_fold(np.zeros(len(labels)), labels, config["num_folds"])
 
-    def run_single_fold(id, fold):
-        """Trains and validates model on single fold."""
+    def run_single_fold(fold_id, fold):
+        """Trains and validates model on single fold.
+
+        Parameters
+        ----------
+        fold_id An identifier for the current fold
+        fold The fold for cross validation
+
+        Returns
+        -------
+        tuple
+            The fold's validation loss and accuracy.
+        """
         # Get training / test indices
         train_index, val_index = fold
 
@@ -124,9 +116,9 @@ def run_tune_params(config):
             train_data=TrainData(train_dataset, val_dataset),
         )
 
-        # Run MLP for the number of specified epochs.
+        # Run classifier for the number of specified epochs.
         best_val_loss, val_acc = model.run(
-            run_id=f"fold-{id}",
+            run_id=f"fold-{fold_id}",
             num_epochs=config["num_epochs"],
             metrics_dir=METRICS_DIR,
             models_dir=MODELS_DIR
@@ -137,10 +129,10 @@ def run_tune_params(config):
     # Process each fold in parallel.
     r = Parallel(n_jobs=-1)(delayed(run_single_fold)(i, fold) for i, fold in enumerate(kfold))
 
+    # Report metrics for the current hyperparameter set.
     val_losses = [metric[0] for metric in r]
     val_accs = [metric[1] for metric in r]
 
-    # Report metrics for the current hyperparameter set.
     session.report({
         "avg_val_loss": np.mean(val_losses),
         "avg_val_acc": np.mean(val_accs)
@@ -148,11 +140,18 @@ def run_tune_params(config):
 
 
 def run_nested_cv(num_samples):
-    """Runs Ray Tuner to search hyperparameter space."""
+    """Runs Tune experiments to search hyperparameter space.
+
+    Parameters
+    ----------
+    num_samples The number of hyperparameter sets to generate
+    """
+    # Define hardware resources per trial.
+    resources = {"cpu": 2, "gpu": 0}
 
     # Define the parameter search configuration.
     config = {
-        "num_folds": tune.choice(np.arange(5, 10)),
+        "num_folds": tune.choice(list(range(5, 10))),
         "num_epochs": tune.choice([250, 500, 750]),
         "neurons_per_layer": tune.choice([128, 256, 512]),
         "num_hidden_layers": tune.choice([2, 3, 4]),
@@ -172,19 +171,24 @@ def run_nested_cv(num_samples):
         run_tune_params,
         config=config,
         search_alg=OptunaSearch(),
-        resources_per_trial={"cpu": 4, "gpu": 0},
+        resources_per_trial=resources,
         metric="avg_val_loss",
         mode="min",
         num_samples=num_samples,
         progress_reporter=reporter,
     )
 
-    # Extract the best trial run from the search.
-    # The best trial is the one with the min avg validation loss.
+    # Extract the best trial (hyperparameter config) from the search.
+    # The best trial is the one with the minimum validation loss for
+    # the folds under analysis.
     best_trial = result.get_best_trial()
     print(f"Best trial config: {best_trial.config}")
     print(f"Best trial validation loss: {best_trial.last_result['avg_val_loss']}")
 
+    # Store best config to file
+    with open(BEST_CONFIG_FILE, "w") as out_file:
+        json.dump(best_trial.config, out_file)
+
 
 if __name__ == "__main__":
-    run_nested_cv(num_samples=10)
+    run_nested_cv(num_samples=1)
