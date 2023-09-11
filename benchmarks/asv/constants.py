f1d352cf483fe4a4ef5cfe27c0380795054dff47
Sandro Campos <scampos@andrew.cmu.edu>
2023-09-11 11:51:45 -0400

    Resolve issue with data import

--- benchmarks/asv/constants.py
+++ benchmarks/asv/constants.py
@@ -2,10 +2,8 @@
 
 import os
 
-BENCH_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
+BENCH_DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data")
 SINGLE_ZTF_ID = "ZTF22abvdwik"
-FIT_FILE = f"{SINGLE_ZTF_ID}.npz"
-SINGLE_ZTF_LIGHTCURVE_COMPRESSED = os.path.join(BENCH_DATA_DIR, FIT_FILE)
-
+SINGLE_ZTF_LIGHTCURVE_COMPRESSED = os.path.join(BENCH_DATA_DIR, f"{SINGLE_ZTF_ID}.npz")
 CLASSIFIER_FILE = os.path.join(BENCH_DATA_DIR, "superphot-model-ZTF23aagkgnz.pt")
 CLASSIFIER_CONF = os.path.join(BENCH_DATA_DIR, "superphot-config-test.yaml")
