a1fa1b43945e9268e651645e8a5f89a4159747c7
Sandro Campos <scampos@andrew.cmu.edu>
2023-09-25 10:56:33 -0400

    Optimize script that generates mosfit data

--- src/superphot_plus/data_generation/mosfit.py
+++ src/superphot_plus/data_generation/mosfit.py
@@ -1,31 +1,74 @@
 import json
 import os
+from argparse import ArgumentParser
+
 import numpy as np
+from tqdm import tqdm
 
 from superphot_plus.file_paths import MOSFIT_DIR, MOSFIT_INPUT_JSON
 from superphot_plus.lightcurve import Lightcurve
 from superphot_plus.samplers.numpyro_sampler import NumpyroSampler
+from superphot_plus.supernova_properties import SupernovaProperties
 from superphot_plus.surveys.surveys import Survey
 
-from superphot_plus.supernova_properties import SupernovaProperties
 
+def read_mosfit_file(mosfit_file):
+    """Reads the mosfit data content from disk.
 
-def import_slsn_mosfit(mosfit_fn, realization):
-    """This is specifically for SLSN code, since we happen to know
-    which parameters actually matter."""
+    Parameters
+    ----------
+    mosfit_file :  str
+        The mosfit file path.
+
+    Returns
+    -------
+    Dict
+        The file content as a dictionary.
+    """
     data = []
-
-    with open(mosfit_fn, encoding="utf-8") as slsn_file:
+    with open(mosfit_file, encoding="utf-8") as slsn_file:
         data = json.load(slsn_file)
-
     top_key = [*data][0]
+    return data[top_key]
 
+
+def format_realization_name(realization):
+    """Determines realization name from its number.
+
+    Parameters
+    ----------
+    realization : int
+        The realization number.
+
+    Returns
+    -------
+    str
+        The name of the light curve realization.
+    """
+    return f"lc_{realization}"
+
+
+def import_slsn_realization(data, realization):
+    """Imports SLSN specific data from mosfit.
+
+    Parameters
+    ----------
+    data : Dict
+        Dictionary containing the mosfit file contents.
+    realization : int
+        The realization / light curve to import.
+
+    Returns
+    -------
+    tuple
+        The light curve and the respective properties.
+    """
     t = []
     flux = []
     err = []
     b = []
 
-    for datum in data[top_key]["photometry"]:
+    for datum in data["photometry"]:
         if realization == int(datum["realization"]):
             # Ignore upper limits
             if "upperlimit" in datum:
@@ -37,7 +80,7 @@ def import_slsn_mosfit(mosfit_fn, realization):
 
     # Also grab parameters...
     # Note that this will ALWAYS be the 0th model, so it is fine to hard code
-    for my_realization in data[top_key]["models"][0]["realizations"]:
+    for my_realization in data["models"][0]["realizations"]:
         if int(my_realization["alias"]) == realization:
             bfield = my_realization["parameters"]["Bfield"]["value"]
             pspin = my_realization["parameters"]["Pspin"]["value"]
@@ -49,69 +92,125 @@ def import_slsn_mosfit(mosfit_fn, realization):
     err = np.asarray(err, dtype=float)
     b = np.asarray(b, dtype=str)
 
-    return t, flux, err, b, bfield, pspin, mejecta, vejecta
+    err = err / np.max(flux)
+    flux = flux / np.max(flux)
+
+    lc = Lightcurve(
+        t,
+        flux,
+        err,
+        b,
+        name=format_realization_name(realization),
+    )
+
+    properties = SupernovaProperties(
+        bfield,
+        pspin,
+        mejecta,
+        vejecta,
+    )
+
+    return lc, properties
 
 
-def generate_mosfit_data(data_file, out_dir, num_realizations):
+def generate_mosfit_data(mosfit_file):
     """Generates the light curve posteriors and stores the mosfit dictionaries
     with the physical parameters for each.
 
-    There are 10000 lightcurves:
-        -> ~40MB mosfit files
-        -> ~120MB posterior files (SVI)
+    Parameters
+    ----------
+    mosfit_file : str
+        The mosfit file path.
     """
-    posteriors_out_dir = os.path.join(out_dir, "posteriors")
-    properties_out_dir = os.path.join(out_dir, "params")
+    # Read mosfit content
+    data = read_mosfit_file(mosfit_file)
 
-    os.makedirs(posteriors_out_dir, exist_ok=True)
-    os.makedirs(properties_out_dir, exist_ok=True)
+    priors = Survey.ZTF().priors
 
-    for realization in np.arange(1, num_realizations + 1):
-        lc_name = f"lc_{realization}"
-        mosfit_file = os.path.join(properties_out_dir, lc_name)
-
-        # Skip existent files
-        if os.path.exists(mosfit_file):
-            print(f"Skipping realization {realization}")
-            continue
-
-        times, flux, err, bands, bfield, pspin, mejecta, vejecta = import_slsn_mosfit(
-            data_file, realization=int(realization)
-        )
-        err = err / np.max(flux)
-        flux = flux / np.max(flux)
-
-        lc = Lightcurve(
-            times,
-            flux,
-            err,
-            bands,
-            name=lc_name,
-        )
+    for i, realization in enumerate(tqdm(realizations)):
+        lc, properties = import_slsn_realization(data, realization=int(i + 1))
 
         sampler = NumpyroSampler()
         posterior_samples = sampler.run_single_curve(
             lc,
             rng_seed=4,
-            priors=Survey.ZTF().priors,
+            priors=priors,
             sampler="svi",
         )
-        posterior_samples.save_to_file(posteriors_out_dir)
 
-        mosfit = SupernovaProperties(
-            bfield,
-            pspin,
-            mejecta,
-            vejecta,
-        )
-        mosfit.write_to_file(mosfit_file)
+        # Save posteriors
+        posterior_samples.save_to_file(posteriors_dir)
 
-        print(f"Progress {(realization/num_realizations*100):.02f}%")
+        # Save properties
+        realization_name = format_realization_name(realization)
+        properties_file = os.path.join(properties_dir, realization_name)
+        properties.write_to_file(properties_file)
+
+
+def get_realizations(num_realizations):
+    """Determines which realizations should be generated.
+
+    Parameters
+    ----------
+    num_realizations: int
+        The maximum number of realizations to have.
+
+    Returns
+    -------
+    tuple
+        A tuple containing a list of missing realization names
+        and a list of the realizations that have previously
+        been generated and will be skipped.
+    """
+    missing_realizations = []
+    skipped_realizations = []
+
+    for realization in np.arange(1, num_realizations + 1):
+        lc_name = f"lc_{realization}"
+        posteriors_fn = f"lc_{realization}_eqwt_svi.npz"
+
+        properties_file = os.path.join(properties_dir, lc_name)
+        posteriors_file = os.path.join(posteriors_dir, posteriors_fn)
+
+        if os.path.exists(properties_file) and os.path.exists(posteriors_file):
+            skipped_realizations.append(realization)
+        else:
+            missing_realizations.append(realization)
+
+    return missing_realizations, skipped_realizations
+
+
+def extract_cmd_args():
+    """Extracts the script command-line arguments."""
+    parser = ArgumentParser(
+        description="Parses mosfit data and generates posteriors and physical property files",
+    )
+    parser.add_argument(
+        "--num_realizations",
+        help="The number of realizations to generate",
+        default=10000,
+    )
+    parser.add_argument(
+        "--mosfit_dir",
+        help="Directory where mosfit data is stored",
+        default=MOSFIT_DIR,
+    )
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
-    generate_mosfit_data(
-        data_file=MOSFIT_INPUT_JSON,
-        out_dir=MOSFIT_DIR,
-        num_realizations=1000,
-    )
+    args = extract_cmd_args()
+
+    # Initialize data directories
+    posteriors_dir = os.path.join(args.mosfit_dir, "posteriors")
+    properties_dir = os.path.join(args.mosfit_dir, "properties")
+
+    os.makedirs(posteriors_dir, exist_ok=True)
+    os.makedirs(properties_dir, exist_ok=True)
+
+    # Realizations to generate
+    realizations, skipped = get_realizations(int(args.num_realizations))
+    print(f"Skipping {len(skipped)} realizations...")
+    print(f"Generating {len(realizations)} missing realizations...")
+
+    generate_mosfit_data(mosfit_file=MOSFIT_INPUT_JSON)
